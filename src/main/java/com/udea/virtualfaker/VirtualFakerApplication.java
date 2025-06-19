718542cdaa224df26afa2852340ea295380190d0
Cristian Penagos <cristian.penagos@udea.edu.co>
2025-06-19 15:47:25 -0500

    start deploy virtual facker

---
+++ src/main/java/com/udea/virtualfaker/VirtualFakerApplication.java
@@ -0,0 +1,13 @@
+package com.udea.virtualfaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualFakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualFakerApplication.class, args);
+    }
+
+}
