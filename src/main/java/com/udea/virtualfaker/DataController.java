07d5a9ca00bec495afdef3991bfed45299c5c86d
Cristian Penagos <cristian.penagos@udea.edu.co>
2025-06-19 17:57:06 -0500

    RestController Construction

---
+++ src/main/java/com/udea/virtualfaker/DataController.java
@@ -0,0 +1,39 @@
+package com.udea.virtualfaker;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController                     //HTTP Request
+public class DataController {
+
+    @GetMapping("/")
+    public String healtCheck(){
+        return "HEALT CHECK OK::: Hola desde el Servidor";
+    }
+    @GetMapping("/version")
+    public String version(){
+        return "Version is 1.0.0::: Mira no mas que chulada";
+    }
+    @GetMapping("/nations")
+    public JsonNode nations(){
+        var objectMapper = new ObjectMapper();
+        var faker = new Faker(new Locale("en-US"));
+        var nations = objectMapper.createArrayNode();
+        for(var i=0; i<10; i++){
+            var nation = faker.nation();
+            nations.add(objectMapper.createArrayNode()
+                    .put("nationality", nation.nationality())
+                            .put("capitalCity", nation.capitalCity())
+                    .put("bandera", nation.flag())
+                    .put("languaje", nation.language()))
+
+        }
+        return nations;
+
+
+    }
+}
